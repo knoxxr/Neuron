eb89dac94bf6e5e24aff32d4546a6aa16d87b756
knoxxr <knoxxr@gmail.com>
2020-08-21 00:27:11 +0900

    modify

---
+++ Brain/OccipitalLobe.cs
@@ -0,0 +1,10 @@
+//OccipitalLobe(후두엽) class
+//시각
+
+public class OccipitalLobe
+{
+    public OccipitalLobe()
+    {
+        
+    }
+}
---
+++ Brain/TemporalLobe.cs
@@ -0,0 +1,10 @@
+//TemporalLobe(측두엽) class
+//기억, 감정
+
+public class TemporalLobe
+{
+    public TemporalLobe()
+    {
+        
+    }
+}
